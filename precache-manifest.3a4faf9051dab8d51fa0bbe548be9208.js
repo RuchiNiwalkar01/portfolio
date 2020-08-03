bb9b6bc1ca2f13c9c29afd83794fb94a6a0994b9
ruchiniwalkar01 <niwalkar.r@northeastern.edu>
2020-08-02 20:29:12 -0400

    Updates

---
+++ precache-manifest.3a4faf9051dab8d51fa0bbe548be9208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d00f0b46530daef9f74cd957867f710",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5a425ed1adc23f82999b",
+    "url": "/portfolio/static/css/main.9c405617.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a425ed1adc23f82999b",
+    "url": "/portfolio/static/js/main.828816cd.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
